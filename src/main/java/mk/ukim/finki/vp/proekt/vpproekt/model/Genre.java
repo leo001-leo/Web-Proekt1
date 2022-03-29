d39f9bb058ebadc9499063bbe1f5eee33091c70b
janazrmanovska <80367195+janazrmanovska@users.noreply.github.com>
2022-03-29 13:22:08 +0200

    Creating id of Genre

--- src/main/java/mk/ukim/finki/vp/proekt/vpproekt/model/Genre.java
+++ src/main/java/mk/ukim/finki/vp/proekt/vpproekt/model/Genre.java
@@ -1,4 +1,20 @@
 package mk.ukim.finki.vp.proekt.vpproekt.model;
 
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Data
+@Entity
 public class Genre {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    public Genre() {
+    }
 }
