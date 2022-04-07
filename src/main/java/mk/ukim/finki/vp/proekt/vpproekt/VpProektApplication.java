b2d5724b767a66a2f8f06af3aae800c01a9846c3
leo001-leo <leo.angelovski@gmail.com>
2022-04-07 14:29:48 +0200

    Dodavanje na password encoder

--- src/main/java/mk/ukim/finki/vp/proekt/vpproekt/VpProektApplication.java
+++ src/main/java/mk/ukim/finki/vp/proekt/vpproekt/VpProektApplication.java
@@ -2,7 +2,10 @@ package mk.ukim.finki.vp.proekt.vpproekt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 @EnableScheduling
@@ -12,4 +15,8 @@ public class VpProektApplication {
         SpringApplication.run(VpProektApplication.class, args);
     }
 
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder(10);
+    }
 }
